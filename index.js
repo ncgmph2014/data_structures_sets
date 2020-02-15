7c6e29f66c08133ae254376822fa3b6f17451ab0
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-14 19:38:22 -0500

    at 17:26 of the data structures tutorial

--- index.js
+++ index.js
@@ -9,48 +9,91 @@ function mySet() {
   this.has = function(element) {
     return collection.indexOf(element) !== -1;
   };
+
+  //this method wil return all the values in a set
+  this.values = function() {
+    return collection;
+  };
+
+  //this method will add an element to the set
+  this.add = function(element) {
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
+    }
+    return false;
+  };
+  //this method will remove an element from a set
+  //in es6 remove is delet
+  this.remove = function(element) {
+    if (this.has(element)) {
+      index = collection.indexOf(element);
+      collection.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
+
+  //this method will reutrn the size of the collection
+  //size is not a method in es6, it's a property
+  this.size = function() {
+    return collection.length;
+  };
+
+  // this method will return the union of the two sets
+  this.union = function(otherSet) {
+    var unionSet = new Set();
+    var firstSet = this.values();
+    var secondSet = otherSet.values();
+    firstSet.forEach(function(e) {
+      unionSet.add(e);
+    });
+    secondSet.forEach(function(e) {
+      unionSet.add(e);
+    });
+    return unionSet;
+  };
+
+  this.intersection = function(otherSet) {
+    var intersectionSet = new mySet();
+    var firstSet = this.values();
+    firstSet.forEach(function(e) {
+      if (otherSet.has(e)) {
+        intersectionSet.add(e);
+      }
+      return intersectionSet;
+    });
+  };
+
+  this.difference = function(otherSet) {
+    var differenceSet = new mySet();
+    var firstSet = this.values();
+    firstSet.forEach(function(e) {
+      if (!otherSet.has(e)) {
+        differenceSet.add(e);
+      }
+    });
+    return differenceSet;
+  };
+
+  //this method will test if the set is a subset of a differnt set
+
+  this.subset = function(otherSet) {
+    var firstSet = this.values();
+    return firstSet.every(function(value) {
+      return otherSet.has(value);
+    });
+  };
 }
 
-//this method wil return all the values in a set
-this.values = function() {
-  return collection;
-};
+var setA = new mySet();
+var setB = new mySet();
+setA.add("a");
+setB.add("b");
+setB.add("c");
+setB.add("a");
+setB.add("d");
 
-//this method will add an element to the set
-this.add = function(element) {
-  if (!this.has(element)) {
-    collection.push(element);
-    return true;
-  }
-  return false;
-};
-//this method will remove an element from a set
-//in es6 remove is delet
-this.remove = function(element) {
-  if (this.has(element)) {
-    index = collection.indexOf(element);
-    collection.splice(index, 1);
-    return true;
-  }
-  return false;
-};
-
-//this method will reutrn the size of the collection
-//size is not a method in es6, it's a property
-this.size = function() {
-  return collection.length;
-};
-
-// this method will return the union of the two sets
-this.union = function(otherSet) {
-  var unionSet = new Set();
-  var firstSet = this.values();
-  var secondSet = otherSet.values();
-  firstSet.forEach(function(e) {
-    unionSet.add(e);
-  });
-  secondSet.forEach(function(e) {
-    unionSet.add(e);
-  });
-  return unionSet;
-};
+console.log(setA.subset(setB));
+//this one doesn't work but i am tired
+console.log(setA.intersection(setB).values());
